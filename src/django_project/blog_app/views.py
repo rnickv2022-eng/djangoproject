4bf1032a3eed762bae6f1d4c79f2275b182e9f9c
Nick <rnickv2022@gmail.com>
2026-01-17 15:59:42 +0300

    Домашняя работа 17-01-2026

--- src/django_project/blog_app/views.py
+++ src/django_project/blog_app/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django_project.blog_app.models import Post
+from django_project.blog_app.models import Category
 from django.shortcuts import get_object_or_404
 
 
@@ -10,12 +11,12 @@ def post_list(request):
     posts = Post.objects.filter(published = True)
     response_content = "<h1>Список статей</h1> <ul>"
     for post in posts:
-       response_content = response_content + f'<li><a href="/post/{post.id}/">{post.title}</a> {post.created_at}</li>'
+       response_content = response_content + f'<li><a href="/post/{post.slug}/">{post.title}</a> {post.created_at}</li>'
     response_content = response_content + "</ul>"
     return HttpResponse(response_content)
 
-def post_detail(request, post_id):
-    post = get_object_or_404(Post, pk = post_id)
+def post_detail(request, post_slug):
+    post = get_object_or_404(Post, slug=post_slug)
     content = f'''
     <h1>{post.title}</h1>
     <p>автор: {post.author.username}</p>
@@ -24,3 +25,31 @@ def post_detail(request, post_id):
     <a href="/posts_list/">Назад к статьям</a>
     '''
     return HttpResponse(content)
+
+def categories_list(request):
+    categories = Category.objects.all()
+    response_content = "<h1>Список категорий</h1> <ul>"
+    for сategory_1 in categories:
+       response_content = response_content + f'<li><a href="/categories/{сategory_1.id}">{сategory_1.title}</a> </li>'
+    response_content = response_content + "</ul>"
+    return HttpResponse(response_content)
+
+def category_detail(request, category_id):
+    сategory_1 = get_object_or_404(Category, pk = category_id)
+    posts = Post.objects.filter(topic=сategory_1)
+    response_content = f"<h1> Категория: {сategory_1.title} </h1> <ul>"
+    response_content += '<li><a href="/categories/">Перейти к категориям </a>'
+    for post in posts:
+        if post.published:
+            post_published = "Да"
+        else:
+            post_published = "Нет"
+        response_content += f'''
+        <h2>{post.title}</h2>
+        <p>автор: {post.author.username}</p>
+        <div> контент:{post.content} </div>
+        <div> публикация:{post_published} </div>
+        <hr>
+        <a href="/post/{post.slug}/">Перейти к статье</a>
+        '''
+    return HttpResponse(response_content)
