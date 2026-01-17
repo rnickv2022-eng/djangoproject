13db0a6d584f3099ce23e2546662c2cf1eb283b2
Nick <rnickv2022@gmail.com>
2026-01-17 18:03:23 +0300

    Исправил замечания

--- src/django_project/blog_app/urls.py
+++ src/django_project/blog_app/urls.py
@@ -6,7 +6,7 @@ app_name = "blog"
 urlpatterns = [
     path("", views.index, name = "index"),
     path("posts_list/", views.post_list, name = "post_list"),
-    path("post/<str:post_slug>/", views.post_detail, name = "post_detail"),
+    path("post/<slug:post_slug>/", views.post_detail, name = "post_detail"),
     path("categories/", views.categories_list, name = "categories_list"),
     path("categories/<int:category_id>/", views.category_detail, name = "category_detail"),
 ]
