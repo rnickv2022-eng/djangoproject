b9e6a5dd24a15651177065fdd00fec024b54236b
Nick <rnickv2022@gmail.com>
2026-02-07 10:51:56 +0300

    Занятие

--- src/django_project/blog_app/urls.py
+++ src/django_project/blog_app/urls.py
@@ -4,13 +4,13 @@ from . import views
 
 app_name = "blog"
 urlpatterns = [
-    path("", views.index, name = "index"),
-    path("posts_list/", views.post_list, name = "post_list"),
-    path("post/<slug:post_slug>/", views.post_detail, name = "post_detail"),
+    path("", views.IndexView.as_view(), name = "index"),
+    path("posts_list/", views.PostListView.as_view(), name = "post_list"),
+    path("post/<slug:post_slug>/", views.PostDetailView.as_view(), name = "post_detail"),
     path("categories/", views.categories_list, name = "categories_list"),
     path("categories/<int:category_id>/", views.category_detail, name = "category_detail"),
     path("create_post/", views.post_create, name="create_post"),
-    path("categories/create/", views.category_create, name="create_category"),
+    path("categories/create/", views.CategoryCreateView.as_view(), name="create_category"),
     path("categories/<int:category_id>/edit", views.category_edit, name = "category_edit"),
     path("post/<slug:post_slug>/edit", views.post_edit, name = "post_edit")
 ]
--- src/django_project/blog_app/views.py
+++ src/django_project/blog_app/views.py
@@ -4,32 +4,30 @@ from django.shortcuts import get_object_or_404, render
 from django_project.blog_app.forms import PostForm,CategoryForm
 from django.shortcuts import redirect
 from django_project.blog_app.management.commands import utils
+from django.views.generic import TemplateView, ListView, DetailView, CreateView
 
+class IndexView(TemplateView):
+    template_name = "blog_app/index.html"
 
-def index(request):
-    posts = Post.objects.filter(published = True).order_by("created_at")[:5]
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["posts"] = Post.objects.filter(published = True).order_by("created_at")[:5]
+        return context
 
-    context = {
-        "posts":posts
-    }
+class PostListView(ListView):
+    model = Post
+    template_name = "blog_app/post_list.html"
+    context_object_name = "posts"
+    paginate_by = 3
 
-    return render(request, template_name="blog_app/index.html", context=context)
+    def get_queryset(self):
+        return self.model.objects.filter(published = True)
 
-def post_list(request):
-    posts = Post.objects.filter(published = True)
-
-    context = {
-        "posts":posts
-    }
-
-    return render(request, template_name="blog_app/post_list.html", context=context)
-
-def post_detail(request, post_slug):
-    post = get_object_or_404(Post, slug=post_slug)
-    context = {
-        "post":post
-    }
-    return render(request, template_name="blog_app/post_detail.html", context=context )
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "blog_app/post_detail.html"
+    context_object_name = "post"
+    slug_url_kwarg = "post_slug"
 
 def categories_list(request):
     categories = Category.objects.all()
@@ -65,22 +63,16 @@ def post_create(request):
     }
     return render(request, "blog_app/create_post.html",context=context)
 
-def category_create(request):
-    if request.method == "POST":
-        form = CategoryForm(request.POST)
 
-        if  form.is_valid():
-            new_category = form.save(commit=False)
-            new_category.slug = utils.translit_1(new_category.title)
-            new_category.save()
-            return redirect("blog:category_detail",new_category.id)
-    else:
-        form = CategoryForm()
+class CategoryCreateView(CreateView):
+    model = Category
+    form_class = CategoryForm
+    template_name = "blog_app/create_category.html"
 
-    context = {
-        "form":form
-    }
-    return render(request, "blog_app/create_category.html",context=context)
+
+    def form_valid(self, form):
+        form.instance.slug = utils.translit_1(form.cleaned_data["title"])
+        return super().form_valid(form)
 
 def category_edit(request,category_id):
     category = get_object_or_404(Category, pk=category_id)
