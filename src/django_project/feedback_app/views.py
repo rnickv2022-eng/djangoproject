8f3e7da61e3c81b5b518e617be25982ab977e286
Nick <rnickv2022@gmail.com>
2026-02-10 20:27:45 +0300

    Домашняя работа 2-я попытка

--- src/django_project/feedback_app/views.py
+++ src/django_project/feedback_app/views.py
@@ -1,21 +1,20 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+
 from django_project.feedback_app.forms import FeedbackForm
 from django_project.feedback_app.models import Feedback
+from django.views.generic import TemplateView, FormView
 
-def feedback_page(request):
-    if request.method == "POST":
-        form = FeedbackForm(request.POST)
+class FeedbackCreateView(FormView):
+    model = Feedback
+    template_name = 'feedback_app/feedback_page.html'
+    form_class = FeedbackForm
+    success_url = reverse_lazy('feedback:success')
+    context_object_name = "post"
 
-        if form.is_valid():
-            data = form.cleaned_data
+    def form_valid(self, form):
+        Feedback.objects.create(**form.cleaned_data)
+        return super().form_valid(form)
 
-            Feedback.objects.create(
-                **data
-            )
 
-            return render(request, "feedback_app/success_page.html")
-
-    else:
-        form = FeedbackForm()
-
-    return render(request, "feedback_app/feedback_page.html",context={"form": form})
+class SuccessFeedbackView(TemplateView):
+    template_name = 'feedback_app/success_page.html'
