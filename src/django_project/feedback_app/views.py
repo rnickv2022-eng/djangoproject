0791991ee0a95e695a2cba05d31338417487bd55
Nick <rnickv2022@gmail.com>
2026-02-10 20:53:09 +0300

    Домашняя работа 3-я попытка

--- src/django_project/feedback_app/views.py
+++ src/django_project/feedback_app/views.py
@@ -1,15 +1,15 @@
 from django.urls import reverse_lazy
 
 from django_project.feedback_app.forms import FeedbackForm
-from django_project.feedback_app.models import Feedback
 from django.views.generic import TemplateView, FormView
 
+from django_project.feedback_app.models import Feedback
+
+
 class FeedbackCreateView(FormView):
-    model = Feedback
     template_name = 'feedback_app/feedback_page.html'
     form_class = FeedbackForm
     success_url = reverse_lazy('feedback:success')
-    context_object_name = "post"
 
     def form_valid(self, form):
         Feedback.objects.create(**form.cleaned_data)
