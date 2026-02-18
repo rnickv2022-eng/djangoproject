4e32ef15631057e80b4078cd6d7775656a25a0e9
Nick <rnickv2022@gmail.com>
2026-02-18 21:35:43 +0300

    Занятие 180226

--- src/django_project/blog_project/settings.py
+++ src/django_project/blog_project/settings.py
@@ -65,6 +65,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "django_project.blog_app.context_processors.categories_processor",
             ],
         },
     },
--- src/django_project/users/apps.py
+++ src/django_project/users/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 
 class UsersConfig(AppConfig):
     name = "django_project.users"
+
+    def ready(self):
+        import django_project.users.signals  # NOQA
