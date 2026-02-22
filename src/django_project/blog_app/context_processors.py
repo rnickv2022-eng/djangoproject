10e0019531c8bcf8a4fb5bab0c366f6bf532ca51
Nick <rnickv2022@gmail.com>
2026-02-23 00:10:06 +0300

    Домашняя работа - дочистил хвосты

--- src/django_project/blog_app/context_processors.py
+++ src/django_project/blog_app/context_processors.py
@@ -1,7 +1,14 @@
-from django_project.blog_app.models import Category
+from django_project.blog_app.models import Category, Post
+from django_project.users.models import Profile
 
 
 def categories_processor(request):
     return {
         'nav_categories': Category.objects.all()
+    }
+
+def blog_stats_processor(request):
+    return {
+        "total_posts": Post.objects.all().count(),
+        "total_users": Profile.objects.all().count()
     }
